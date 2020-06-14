9ff88eb71e2c707ecce7cd3fb1093d05bd176367
Mahin <mahinmalhotra20@gmail.com>
2020-06-15 04:29:56 +0530

    commit 2

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/home')
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '<h1>Hello, World!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
