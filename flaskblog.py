688b5d9643bdbdec90341dd08391e829aa6742c1
Mahin <mahinmalhotra20@gmail.com>
2020-06-16 06:28:22 +0530

    added templates and static files

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author' : 'Mahin Malhotra',
+        'title' : ' get schwifty',
+        'content' : 'wubba lubba dub dub',
+        'date_posted' : ' 16 June 2020'
+    },
+    {
+        'author' : 'Darth Vader',
+        'title' : ' Sith are awesome',
+        'content' : 'join the dark side',
+        'date_posted' : ' 16 June 2020'
+
+    }
+]
+
 @app.route('/home')
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
-
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
