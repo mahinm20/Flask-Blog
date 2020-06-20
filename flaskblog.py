b6d5c92073cbc1cbe43dfd4e5a5d4079666a12a9
Mahin <mahinmalhotra20@gmail.com>
2020-06-20 06:24:37 +0530

    Made login and register functional pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-
+import email_validator
 
 
 app = Flask(__name__)
@@ -22,24 +22,39 @@ posts = [
     }
 ]
 
-@app.route('/home')
-@app.route('/')
-def hello():
-    return render_template('home.html',posts=posts)
 
-@app.route('/about')
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html',form=form,title='Register') 
- 
-# @app.route('/login')
-# def register():
-#     form = LoginForm()
-#     return render_template('login.html',form=form,title='login')
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,30 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-
-class LoginForm(FlaskForm):
-    
-    email = StringField('Email',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-    
-
-     
-
-""" from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+import email_validator
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -42,4 +19,4 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login') """
+    submit = SubmitField('Login')
