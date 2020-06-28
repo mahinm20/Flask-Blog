d021fcc10c28825da0970c3024bbbe3c23faffe0
Mahin <mahinmalhotra20@gmail.com>
2020-06-29 01:44:31 +0530

    added login /logout

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 import email_validator
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b89285b701204bd2bad83b2e2f5b3ace'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
 
 from flaskblog import routes
